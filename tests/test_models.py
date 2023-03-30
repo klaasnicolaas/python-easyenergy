24ead39e9ae6366475296f5516438f2f8036664c
Sander Striker <s.striker@striker.nl>
2023-03-31 01:13:21 +0200

    Add property to run energy consumers at the best time (#55)
    * Add two properties for use in HomeAssistant giving the ability to
    run energy consumers in the cheapest N hours.
    
    * Fix linting.  Rename functions to be clearer of intent.
    
    * Add tests for new values to reach coverage.
    
    * Rename properties to reflect what they compute.
    
    * Add new property to README.
    
    * Update test value.

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,6 +49,8 @@ async def test_electricity_model_usage(aresponses: ResponsesMockServer) -> None:
         ).replace(tzinfo=timezone.utc)
         assert energy.pct_of_max_usage == 89.85
         assert isinstance(energy.timestamp_usage_prices, list)
+        assert energy.hours_priced_equal_or_lower_usage == 21
+        assert energy.hours_priced_equal_or_higher_return == 5
 
 
 @pytest.mark.freeze_time("2022-12-29 15:00:00+01:00")
